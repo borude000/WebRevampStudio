c6d6f61649e0be0a34c6922277e630a458be2210
Nikhil Borude <nikhilborude000@gmail.com>
2025-08-27 13:26:42 +0530

    Resolve merge conflicts

--- server/index.ts
+++ server/index.ts
@@ -56,12 +56,8 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Other ports are firewalled. Default to 5000 if not specified.
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen(port, "127.0.0.1", () => {
-    log(`serving on port ${port}`);
+  const PORT = process.env.PORT || 10000;
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`[express] serving on port ${PORT}`);
   });
 })();
